69f76f6596474763e7bfb28d08e40e8a6cc40a9e
nabdeepkour <77386987+nabdeepkour@users.noreply.github.com>
2021-03-20 16:06:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+	
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1000,200,30);
+	mango3=new mango(1200,300,30);
+	mango4=new mango(1000,100,30);
+	mango5=new mango(900,200,30);
+	mango6=new mango(1100,200,30);
+	mango7=new mango(1100,300,30);
+	mango8=new mango(1000,300,30);
+	mango9=new mango(1000,80,20);
+	mango10=new mango(1200,200,30);
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);	
+	stoneObj = new Stone(900,30,20);
+	launcherObject=new Launcher(stoneObj.body,{x:235,y:420})
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+mango2.display();
+mango3.display();
+mango4.display();
+mango5.display();
+mango6.display();
+mango7.display();
+mango8.display();
+mango9.display();
+mango10.display();
+  groundObject.display();
+  stoneObj.display();
+  launcherObject.display();
+  detectollision(stoneObj,mango1);
+  detectollision(stoneObj,mango2);
+  detectollision(stoneObj,mango3);
+  detectollision(stoneObj,mango4);
+  detectollision(stoneObj,mango5);
+  detectollision(stoneObj,mango6);
+  detectollision(stoneObj,mango7);
+  detectollision(stoneObj,mango8);
+  detectollision(stoneObj,mango9);
+  detectollision(stoneObj,mango10);
+
+
+
+
+
+}
+
+function mouseDragged()
+{
+	Matter.Body.setPosition(stoneObj.body, {x:mouseX, y:mouseY}) 
+}
+function mouseReleased()
+{
+	launcherObject.fly();
+}
+function detectollision(lstone,lmango){
+	 mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+	
+	var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r)
+	  {
+		//console.log(distance);
+		  Matter.Body.setStatic(lmango.body,false);
+	  }
+  
+	}
